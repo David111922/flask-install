c0700dee1c6ee20aa76132651235b2eae67f81ca
David111922 <salve62@yahoo.com>
2024-08-26 20:02:22 -0700

    issue with template

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -9,8 +9,14 @@ def create_app():
     @app.route('/')
     def index(): 
         return 'Hello, PetFax!'
-# register pet blueprint
-    from . import pet
+
+    # register pet blueprint 
+    from . import pet 
     app.register_blueprint(pet.bp)
+
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
     # return the app 
     return app
